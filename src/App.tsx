467a074e33e229c534e2a34a0057914baffa5521
Gamadiel-hm <gamadiel-hm@outlook.es>
2024-04-23 16:16:21 -0600

    create store for join group for url params

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,53 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { HubConnectionBuilder } from "@microsoft/signalr";
 import { useNotificationStore } from "./context/notification.store";
 import { Notification } from "./pages/notification/notification.d";
+import { useJoinGroup } from "./context/joinGroup.store";
 
 function App() {
-  const setNewNotification = useNotificationStore(state => state.setNotificationOne);
+  const [newGroupState, setNewGroupState] = useState<string>();
+  const setNewNotification = useNotificationStore(
+    (state) => state.setNotificationOne
+  );
+  const newGRoup = useJoinGroup(state => state.joinGroup)
 
   useEffect(() => {
     const conn = new HubConnectionBuilder()
       .withUrl(import.meta.env.VITE_BASE_URL_HUB)
       .build();
 
-    conn.start()
-      .then(() => conn.invoke("JoinGroup", { userName: "Carlos", Room: "Beer", Message:"Join Group"}))
+    setNewGroupState(group => group = newGRoup)
+
+    conn
+      .start()
+      .then(() =>
+        conn.invoke("JoinGroup", {
+          userName: "Carlos",
+          Room: newGRoup,
+          Message: "Join Group",
+        })
+      )
       .then(() => console.log("Join Group"))
       .catch((error) => console.log(error.message));
 
     conn.on("JoinGroupMessage", (message: string) => console.log(message));
     conn.on("groupMessage", (message: string, userName: string) => {
-      const newMessage: Notification = {userId: "" ,message: message, group: "Beer", userName: userName, date: new Date, location: "" }
+      const newMessage: Notification = {
+        userId: "",
+        message: message,
+        group: "Beer",
+        userName: userName,
+        date: new Date(),
+        location: "",
+      };
       setNewNotification(newMessage);
+      console.log("new message");
     });
-    
-
-  }, [])
+  }, [newGroupState]);
   return (
     <>
-      <header className="container-menu">
-        <nav className="menu-container">
-          <div>
-            <Link to="/notification">Notification</Link>
-          </div>
-          <div>
-            <Link to="/notification">List</Link>
-          </div>
-        </nav>
-        <h3>App Front EuCotton</h3>
-      </header>
       <Outlet />
     </>
   );
