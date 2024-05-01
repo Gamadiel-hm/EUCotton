f15ce52f778b14452cda5a35b18c809b416f3a99
Gamadiel <gamadiel-hm@hotmail.com>
2024-05-01 16:04:16 -0600

    merge branch main => development

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { Outlet } from "react-router-dom";
-import "./App.css";
-import { useEffect } from "react";
-import { HubConnectionBuilder } from "@microsoft/signalr";
-import { useNotificationStore } from "./context/notification.store";
-import { NotificationFetch } from "./pages/notification/types/notification";
-import { useJoinGroup } from "./context/joinGroup.store";
+import { Outlet } from 'react-router-dom';
+import './App.css';
+import { useEffect } from 'react';
+import { HubConnectionBuilder } from '@microsoft/signalr';
+import { useNotificationStore } from './context/notification.store';
+import { NotificationFetch } from './pages/notification/types/notification';
+import { useJoinGroup } from './context/joinGroup.store';
 
 function App() {
   const setNewNotification = useNotificationStore(
-    (state) => state.setNotificationOne
+    state => state.setNotificationOne
   );
-  const newGRoup = useJoinGroup((state) => state.joinGroup);
+  const newGRoup = useJoinGroup(state => state.joinGroup);
 
   useEffect(() => {
     const conn = new HubConnectionBuilder()
@@ -20,17 +20,17 @@ function App() {
     conn
       .start()
       .then(() =>
-        conn.invoke("JoinGroup", {
-          userName: "Carlos",
+        conn.invoke('JoinGroup', {
+          userName: 'Carlos',
           Room: newGRoup,
-          Message: "Join Group",
+          Message: 'Join Group'
         })
       )
-      .then(() => console.log("Join Group"))
-      .catch((error) => console.log(error.message));
+      .then(() => console.log('Join Group'))
+      .catch(error => console.log(error.message));
 
-    conn.on("JoinGroupMessage", (message: string) => console.log(message));
-    conn.on("groupMessage", (newNotification: NotificationFetch) => {
+    conn.on('JoinGroupMessage', (message: string) => console.log(message));
+    conn.on('groupMessage', (newNotification: NotificationFetch) => {
       setNewNotification(newNotification);
     });
   }, [newGRoup]);
