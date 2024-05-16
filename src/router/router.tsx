5ca27aedfc2dfaa3a107d5151ed6856653febc46
Gamadiel-hm <gamadiel-hm@outlook.es>
2024-05-16 16:15:11 -0600

    Add base url for react router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,27 +1,30 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Routes_Public } from './routerConst';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    async lazy() {
-      const { default: App } = await import('../App');
-      return {
-        Component: App
-      };
-    },
-    children: [
-      {
-        path: `${Routes_Public.notification}/:userId/:room/:roomId`,
-        async lazy() {
-          const { NotificationPage } = await import(
-            '../pages/notification/notificationPage'
-          );
-          return {
-            Component: NotificationPage
-          };
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      async lazy() {
+        const { default: App } = await import('../App');
+        return {
+          Component: App
+        };
+      },
+      children: [
+        {
+          path: `${Routes_Public.notification}/:userId/:room/:roomId`,
+          async lazy() {
+            const { NotificationPage } = await import(
+              '../pages/notification/notificationPage'
+            );
+            return {
+              Component: NotificationPage
+            };
+          }
         }
-      }
-    ]
-  }
-]);
+      ]
+    }
+  ],
+  { basename: process.env.PUBLIC_URL }
+);
