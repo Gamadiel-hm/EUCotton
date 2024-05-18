15e0238452bffb3c05d8972639222fff2901b7c5
Gamadiel <gamadiel-hm@hotmail.com>
2024-05-18 10:31:36 -0600

    Add branch deploy with new router configure

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,32 +1,29 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import { Routes_Public } from './routerConst';
 import ErrorPage from '../ErrorPage';
 
-export const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      async lazy() {
-        const { default: App } = await import('../App');
-        return {
-          Component: App
-        };
-      },
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: `${Routes_Public.notification}/:userId/:room/:roomId`,
-          async lazy() {
-            const { NotificationPage } = await import(
-              '../pages/notification/notificationPage'
-            );
-            return {
-              Component: NotificationPage
-            };
-          }
+export const router = createHashRouter([
+  {
+    path: '/',
+    async lazy() {
+      const { default: App } = await import('../App');
+      return {
+        Component: App
+      };
+    },
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: `${Routes_Public.notification}/:userId/:room/:roomId`,
+        async lazy() {
+          const { NotificationPage } = await import(
+            '../pages/notification/notificationPage'
+          );
+          return {
+            Component: NotificationPage
+          };
         }
-      ]
-    }
-  ],
-  { basename: import.meta.env.VITE_BASE_URL_ROUTER }
-);
+      }
+    ]
+  }
+]);
